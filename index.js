64a433923c7f7228b9f75833407fb6357a83212b
Tejan Singh <tstejan@gmail.com>
2022-09-07 13:44:39 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+var readlineSync = require("readline-sync");
+
+const questions = [
+  {
+    question: "Where do I live? ",
+    answer: "jaipur",
+  },
+
+  {
+    question: "Who is my favourite superhero? ",
+    answer: "ironman",
+  },
+
+  {
+    question: "When did I complete my M. Tech degree ?",
+    answer: "2021",
+  },
+];
+
+var score = 0;
+
+var scores = {
+  currentScore: score,
+  HighestScore: {
+    name: "Tejan",
+    score: 5,
+  },
+};
+
+console.log(
+  "Welcome to Quiz App! \nHere you will answer questions related to me. \n"
+);
+
+const quiz = (question, correctAnswer) => {
+  const userAnswer = readlineSync.question(`${question} \n`);
+  if (userAnswer === correctAnswer) {
+    console.log("You are right!");
+    score = score + 1;
+    scores.currentScore = score;
+    console.log(`Current score is ${score} \n`);
+  } else {
+    console.log("You are wrong!");
+    console.log(`Current score is ${score} \n`);
+  }
+};
+
+for (var i = 0; i < questions.length; i++) {
+  console.log(`Question no. ${i + 1}`);
+  quiz(questions[i].question, questions[i].answer);
+}
+
+console.log(`Final score is ${scores.currentScore}`);
+console.log(
+  `Highest score is ${scores.HighestScore.score} created by ${scores.HighestScore.name} \n`
+);
+
+if (scores.currentScore >= scores.HighestScore.score) {
+  console.log(
+    "Congrats! You have beaten the highest score. Please share your screenshot to update the highest score"
+  );
+}
